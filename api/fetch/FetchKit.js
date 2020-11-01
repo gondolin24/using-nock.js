fd1b599f9ef9e23c3db2a5efcb31d14e0fd90166
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-11-01 17:24:56 -0500

    refector

--- api/fetch/FetchKit.js
+++ api/fetch/FetchKit.js
@@ -4,7 +4,6 @@ class FetchKit {
 
     async getRandomApi() {
         const resp = await fetch('https://wikimedia.org/api/rest_v1/?spec', {
-            // These properties are part of the Fetch Standard
             method: 'GET'
         })
 
