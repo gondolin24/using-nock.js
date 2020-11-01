5021274d9c1532a7d7d71564fe9e0ceea07d4569
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-11-01 17:08:44 -0500

    getting a quick api up

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/carrots', function(req, res, next) {
+  res.json({
+    color:'orange'
+  })
+});
+router.get('/apples', function(req, res, next) {
+  res.json({
+    color:'green'
+  })
 });
 
 module.exports = router;
