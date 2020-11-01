c54f716b256690bb48058a401ec4c0a10d0f7a5b
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-11-01 17:29:52 -0500

    Adding first mock

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,26 @@
+const nock = require('nock')
 var express = require('express');
 var router = express.Router();
 const FetchKit = require('../fetch/FetchKit')
 
 
-router.get('/carrots', function(req, res, next) {
-  res.json({
-    color:'orange'
-  })
+router.get('/carrots', function (req, res, next) {
+    res.json({
+        color: 'orange'
+    })
 });
-router.get('/apples', async function(req, res, next) {
-  const fetchKit = new FetchKit()
-  const response = await fetchKit.getRandomApi()
-  await res.json(response)
+router.get('/apples', async function (req, res, next) {
+
+    nock('https://wikimedia.org')
+        .get('/api/rest_v1/?spec')
+        .reply(200, {
+            test: 'nock'
+        })
+
+
+    const fetchKit = new FetchKit()
+    const response = await fetchKit.getRandomApi()
+    await res.json(response)
 });
 
 module.exports = router;
