56938c68ecb174834b6e45cb0e8dd147aef2d1a0
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-11-01 20:51:52 -0500

    junk

--- api/fetch/FetchKit.js
+++ api/fetch/FetchKit.js
@@ -3,8 +3,13 @@ const fetch = require('node-fetch')
 class FetchKit {
 
     async getRandomApi() {
-        const resp = await fetch('https://wikimedia.org/api/rest_v1/?spec', {
-            method: 'GET'
+        const resp = await fetch('https://en.wikipedia.org/api/rest_v1/page/random/summary', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'content-encoded': 'deflate, gzip',
+                "Accept": "application/json,text/*;q=0.99"
+            }
         })
 
         return resp.json()
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,26 +1,37 @@
 const nock = require('nock')
+const fs = require('fs')
 var express = require('express');
 var router = express.Router();
 const FetchKit = require('../fetch/FetchKit')
+const {decode} = require('./Decoder')
 
-
-router.get('/carrots', function (req, res, next) {
-    res.json({
-        color: 'orange'
-    })
-});
-router.get('/apples', async function (req, res, next) {
-
+router.get('/nock-run=black-box', function (req, res, next) {
     nock('https://wikimedia.org')
-        .get('/api/rest_v1/?spec')
+        .get('/api/rest_v1/page/random/summary')
         .reply(200, {
             test: 'nock'
         })
 
+});
+router.get('/nock-record', async function (req, res, next) {
+    nock.recorder.rec({
+        output_objects: true
+    })
 
     const fetchKit = new FetchKit()
     const response = await fetchKit.getRandomApi()
-    await res.json(response)
+    nock.restore()
+    const nockCalls = nock.recorder.play()
+    await nockCalls.forEach(async (item) => {
+        const {status, response} = item
+        if (status === 200) {
+            const rr = decode(response)
+            await fs.writeFileSync(`${__dirname}+/temp.json`, JSON.stringify(rr), function () {
+            })
+        }
+    })
+
+    res.json(response)
 });
 
 module.exports = router;
